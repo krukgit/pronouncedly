a0ae22367281746873a29d865f9e9491ae6aa009
Maciej Kruk <mkruk@telr.com>
2015-03-12 23:05:41 +0800

    created languages

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150307142253) do
+ActiveRecord::Schema.define(version: 20150312145717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "languages", force: :cascade do |t|
+    t.string "name"
+    t.string "native_name"
+    t.string "code"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+languages = CSV.read 'db/data/languages.csv'
+languages.map!{ |l| {name: l[0], native_name: l[1], code: l[2]} }
+Language.create languages
